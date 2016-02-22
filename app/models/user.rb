1b0da36cb67da4b2d7af358d038f213882a6a032
Andrew Henrique <andrewhenriquef@gmail.com>
2016-02-22 13:29:24 -0300

    added some validations to user, need to encryt password to database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+
+    validates :name, :email, :password, presence: true
+    validates :email, uniqueness: true
+    
+    validates :password, length: { in: 6..20 }
+
 end
